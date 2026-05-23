9cff2262d41ec8841ad69b986dcb02b503cd37bf
Atik03 <atikshaharia03@gmail.com>
2026-05-23 18:42:31 +0600

    add jwt token

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
+
 const uri = process.env.MONGODB_URI;
 
 const app = express();
@@ -21,6 +23,26 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
 
 async function run() {
   try {
@@ -30,12 +52,12 @@ async function run() {
     const AppointmentsCollection = db.collection("allDoctorList");
     const bookingCollection = db.collection("bookings");
 
-    app.get("/allDoctorList", async (req, res) => {
+    app.get("/allDoctorList", verifyToken, async (req, res) => {
       const result = await AppointmentsCollection.find().toArray();
       res.json(result);
     });
 
-    app.get("/allDoctorList/:id", async (req, res) => {
+    app.get("/allDoctorList/:id", verifyToken, async (req, res) => {
       const { id } = req.params;
 
       const result = await AppointmentsCollection.findOne({
@@ -54,14 +76,14 @@ async function run() {
       res.json(result);
     });
 
-    app.post("/booking", async (req, res) => {
+    app.post("/booking", verifyToken, async (req, res) => {
       const bookingData = req.body;
       const result = await bookingCollection.insertOne(bookingData);
 
       res.json(result);
     });
 
-    app.get("/booking/:userId", async (req, res) => {
+    app.get("/booking/:userId", verifyToken, async (req, res) => {
       const { userId } = req.params;
 
       const result = await bookingCollection.find({ userId: userId }).toArray();
@@ -69,10 +91,9 @@ async function run() {
       res.json(result);
     });
 
-    app.patch("/booking/:userBookingId", async (req, res) => {
+    app.patch("/booking/:userBookingId", verifyToken, async (req, res) => {
       const { userBookingId } = req.params;
       const updatedData = req.body;
-      console.log(updatedData);
 
       const result = await bookingCollection.updateOne(
         { _id: new ObjectId(userBookingId) },
@@ -82,7 +103,7 @@ async function run() {
       res.json(result);
     });
 
-    app.delete("/booking/:bookingId", async (req, res) => {
+    app.delete("/booking/:bookingId", verifyToken, async (req, res) => {
       const { bookingId } = req.params;
       const result = await bookingCollection.deleteOne({
         _id: new ObjectId(bookingId),
